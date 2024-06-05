2a4b346f02b4863a3555c83f09dce9d9ed51214b
Beni Bachmann <beni.bachmann@gmail.com>
2024-06-05 13:28:15 +0200

    feat: auth

--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -8,6 +8,8 @@ mod route;
 mod task;
 mod traits;
 
+use std::fs;
+
 use axum::http::{
     header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
     HeaderValue, Method,
@@ -17,13 +19,15 @@ use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
+    let bearer_token = fs::read_to_string("bearer_token.txt").unwrap();
+
     let cors = CorsLayer::new()
         .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
-    let app = create_router().layer(cors);
+    let app = create_router(bearer_token).layer(cors);
 
     println!("🚀 Server started successfully");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
--- backend-rs/src/route.rs
+++ backend-rs/src/route.rs
@@ -1,13 +1,21 @@
+use std::fs;
+
 use axum::{
     routing::{delete, get, patch, post},
     Router,
 };
 
 use crate::{
-    agent::{create_agent, delete_agent, read_agent, update_agent}, check::check_task, handler::health_checker_handler, model, task::{create_task, read_task, read_tasks}
+    agent::{create_agent, delete_agent, read_agent, update_agent},
+    check::check_task,
+    handler::health_checker_handler,
+    model,
+    task::{create_task, read_task, read_tasks},
 };
 
-pub fn create_router() -> Router {
+use tower_http::validate_request::ValidateRequestHeaderLayer;
+
+pub fn create_router(bearer_token: String) -> Router {
     let db = model::todo_db();
 
     Router::new()
@@ -20,6 +28,7 @@ pub fn create_router() -> Router {
         .route("/api/task", get(read_tasks))
         .route("/api/task/:id", get(read_task))
         .route("/api/task/:id/check", post(check_task))
+        .route_layer(ValidateRequestHeaderLayer::bearer(&bearer_token))
 
     // .route(
     //     "/api/todos",
